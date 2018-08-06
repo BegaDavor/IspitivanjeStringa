cd0a661d9a297439267207abe8c9ec5ba20d6591
Davor Beganovic <beganovic.davor@gmail.com>
2018-08-06 15:21:43 +0200

    Aplikacija za ispitivanje nekih osobina unesenog stringa

---
+++ src/Metode.java
@@ -0,0 +1,34 @@
+
+public class Metode {
+	
+	//Atributi
+	
+	public String recenica;
+	
+	//Ponasanje
+	
+	public int duzina(String recenica) {
+		// Implementirati metodu koja vraca duzinu stringa
+	}
+	
+	public void karakteriNaNeparnimPozicijama(String recenica) {
+		//Implementirati metodu koja ispisuje karaktere na neparnim pozicijama
+	}
+	
+	public void karakteriNaParnimPozicijama(String recenica) {
+		//Implementirati metodu koja ispisuje sve karaktere na parnim pozicijama
+	}
+	
+	public int uppercaseKarakteri(String recenica) {
+		//Iplementirati metodu koja vraca broj uppercase karaktera
+	}
+	
+	public int lowercaseKarakteri(String recenica) {
+		//Implementirati metodu koja vraca broj lowercase karaktera
+	}
+	
+	public void karakteriKojiNisuSlova(String recenica) {
+		//Implementirati metodu koja vraca sve karaktere koji nisu slova
+	}
+
+}
---
+++ src/Test.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Metode metode = new Metode();
+		
+		System.out.println("Unesite neki string: ");
+		
+		String recenica = input.nextLine();
+		
+		System.out.println("Duzina stringa je: " + metode.duzina(recenica));
+		System.out.print("Karakteri na neparnim pozicijama su: ");
+		metode.karakteriNaNeparnimPozicijama(recenica);
+		System.out.print("/nKarakteri na parnim pozicijama su: ");
+		metode.karakteriNaParnimPozicijama(recenica);
+		System.out.println("Broj uppercase karaktera je: " + metode.uppercaseKarakteri(recenica));
+		System.out.println("Broje lowercase karaktera je: " + metode.lowercaseKarakteri(recenica));
+		System.out.print("/nKarakteri koji nisu slova su: ");
+		metode.karakteriKojiNisuSlova(recenica);
+		
+		input.close();
+	}
+
+}
