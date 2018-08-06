faf6ac963ee574da9ac5e98d8f7e636e734e8b8d
Davor Beganovic <beganovic.davor@gmail.com>
2018-08-06 17:49:04 +0200

    Aplikacija koja ispituje odredjene osobine nekog stringa

--- src/Test.java
+++ src/Test.java
@@ -13,11 +13,11 @@ public class Test {
 		System.out.println("Duzina stringa je: " + metode.duzina(recenica));
 		System.out.print("Karakteri na neparnim pozicijama su: ");
 		metode.karakteriNaNeparnimPozicijama(recenica);
-		System.out.print("/nKarakteri na parnim pozicijama su: ");
+		System.out.print("\nKarakteri na parnim pozicijama su: ");
 		metode.karakteriNaParnimPozicijama(recenica);
 		System.out.println("Broj uppercase karaktera je: " + metode.uppercaseKarakteri(recenica));
 		System.out.println("Broje lowercase karaktera je: " + metode.lowercaseKarakteri(recenica));
-		System.out.print("/nKarakteri koji nisu slova su: ");
+		System.out.print("\nKarakteri koji nisu slova su: ");
 		metode.karakteriKojiNisuSlova(recenica);
 		
 		input.close();
